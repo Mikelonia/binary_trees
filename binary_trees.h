4b917e66a5a53fddaf94a14bbcef5b4f50953128
Mikelonia <onajitemike4real@gmail.com>
2023-06-05 13:56:25 -0700

    Task 132

--- binary_trees.h
+++ binary_trees.h
@@ -67,7 +67,8 @@ avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 int binary_tree_is_heap(const binary_tree_t *tree);
-heap_t *heap_insert(heap_t **root, int value)
+heap_t *heap_insert(heap_t **root, int value);
+	heap_t *array_to_heap(int *array, size_t size);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);

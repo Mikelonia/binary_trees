0a4d6174c8b3a331cdc4f259940853f69413f9af
Mikelonia <onajitemike4real@gmail.com>
2023-06-05 12:54:41 -0700

    Task 112-array_to_bst.c

--- binary_trees.h
+++ binary_trees.h
@@ -58,6 +58,8 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);

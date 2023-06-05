75fb6060bbe6bac4a37cee382b6a18dcbc63487b
Mikelonia <onajitemike4real@gmail.com>
2023-06-05 13:34:34 -0700

    Task  124-sorted_array_to_avl.c

--- binary_trees.h
+++ binary_trees.h
@@ -65,6 +65,7 @@ int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);

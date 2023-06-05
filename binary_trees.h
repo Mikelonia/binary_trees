9fb1035084029e23c116fa36d55c80a8ee667ffb
Mikelonia <onajitemike4real@gmail.com>
2023-06-05 13:29:31 -0700

    Task 123-avl_remove.c

--- binary_trees.h
+++ binary_trees.h
@@ -64,6 +64,7 @@ bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);

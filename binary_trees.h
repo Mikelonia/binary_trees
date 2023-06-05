974f5cd77c23421d9adcc8f8b6ddb3faa4dabb7a
Mikelonia <onajitemike4real@gmail.com>
2023-06-05 13:10:21 -0700

    Task 120-binary_tree_is_avl.c

--- binary_trees.h
+++ binary_trees.h
@@ -61,6 +61,7 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
